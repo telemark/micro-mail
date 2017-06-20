b8e7357b0c2c72dca04010174f6bac5800d83118
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-06-20 15:38:24 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
+[![Build Status](https://travis-ci.org/telemark/micro-mail.svg?branch=master)](https://travis-ci.org/telemark/micro-mail)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+[![Greenkeeper badge](https://badges.greenkeeper.io/telemark/micro-mail.svg)](https://greenkeeper.io/)
+
 # micro-mail
 Microservice for sending emails
+
+## API
+
+All calls must supply a valid jwt
+
+### ```POST /mail```
+
+Send sms
+
+```JavaScript
+{
+  from: '<from-mail>',
+  to: '<to-mail>',
+  subject: '<subject>',
+  text: '<plain-text>',
+  cc: '<cc-mail>', // Optional
+  bcc: '<bcc-mail>', // Optional
+  replyTo: '<replyTo-mail>', // Optional
+  html: '<html>' // Optional
+}
+```
+
+```bash
+$ curl -v -H "Authorization: <INSERT TOKEN>" -d '{ "from": "from@example.com", "to": "to@example.com", "subject": "Do you read me?", "text": "Loud and clear!" }' https://mail.service.io/mail
+```
+
+## License
+
+[MIT](LICENSE)
+
+![Robohash image of micro-mail](https://robots.kebabstudios.party/micro-mail.png "Robohash image of micro-mail")
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const readFileSync = require('fs').readFileSync
+const marked = require('marked')
+const { parse } = require('url')
+const { json, send } = require('micro')
+const verifyJwt = require('./lib/verify-jwt')
+const sendMail = require('./lib/send-mail')
+const logger = require('./lib/logger')
+
+module.exports = async (request, response) => {
+  const { pathname, query } = await parse(request.url, true)
+
+  if (pathname === '/mail') {
+    const data = request.method === 'POST' ? await json(request) : query
+    const verified = await verifyJwt(request)
+    logger('info', ['index', 'mail', 'start'])
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
+    if (verified.isValid === true) {
+      logger('info', ['index', 'mail', 'verified'])
+      try {
+        const result = await sendMail(data)
+        logger('info', ['index', 'mail', 'success'])
+        send(response, 200, result)
+      } catch (error) {
+        logger('error', ['index', 'mail', error])
+        send(response, 500, error)
+      }
+    } else {
+      logger('error', ['index', 'mail', 'Invalid token'])
+      send(response, 401, new Error('Invalid token'))
+    }
+  } else {
+    const readme = readFileSync('./README.md', 'utf-8')
+    const html = marked(readme)
+    send(response, 200, html)
+  }
+}

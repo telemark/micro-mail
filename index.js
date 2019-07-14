5acdced58f68f1fbd162d17db2851b94edcfcea4
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-07-14 08:27:39 +0200

    Moves to pure ZEIT/Now (major)

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const { json, send } = require('micro')
 const sendMail = require('./lib/send-mail')
 const logger = require('./lib/logger')
 
 async function deliverMail (request, response) {
-  const data = await json(request)
+  const data = request.body
   try {
     const result = await sendMail(data)
     logger('info', ['index', 'deliverMail', 'success'])
-    send(response, 200, result)
+    response.json(result)
   } catch (error) {
     logger('error', ['index', 'deliverMail', error])
-    send(response, 500, error)
+    response.status(500)
+    response.send(error)
   }
 }
 
